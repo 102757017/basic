3b1ab688af8afebd86ceaf2c9744bb3b2f9add75
102757017 <sdf63fg@yeah.net>
2017-10-08 18:19:33 +0800

    PC

---
+++ 图片处理/腐蚀膨胀.py
@@ -0,0 +1,56 @@
+# -*- coding: UTF-8 -*-
+import cv2
+import os
+import numpy as np
+
+
+os.chdir(os.path.dirname(__file__))
+
+ 
+img = cv2.imread('text.png')
+
+#图片先转成灰度的
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+cv2.imshow("gray",gray)
+
+
+#再把图片转换为二值图
+ret, binary = cv2.threshold(gray,127,255,cv2.THRESH_BINARY)  
+cv2.imshow("threshold",binary)
+
+
+#图片反色
+thresh_img = cv2.bitwise_not(binary)
+cv2.imshow("inverse",thresh_img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+scale = 15
+#img.shape返回的是图像的行数，列数，色彩通道数.
+h_size = int(thresh_img.shape[1]/scale)
+
+
+
+'''
+构造形态学因子，形态学因子类似于笔刷，有不同的形状，并且有一个锚点
+此处构造了一个10*1的矩阵，形态学因子是矩形，锚点未定义时取中心值
+常用的形态学因子有如下几种：
+椭圆（MORPH_ELLIPSE）
+十字形结构（MORPH_CROSS）
+
+'''
+h_structure = cv2.getStructuringElement(cv2.MORPH_RECT,(10,1))
+#腐蚀图像，相当于将笔刷沿轮廓内边缘绕行，返回笔刷锚点构成的封闭区
+img1 = thresh_img.copy()
+h_erode_img = cv2.erode(img1,h_structure,1)
+cv2.imshow("erode",h_erode_img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+#膨胀图像，相当于将笔刷沿轮廓外边缘绕行，返回笔刷锚点构成的封闭区
+img2 = thresh_img.copy()
+h_dilate_img = cv2.dilate(img2,h_structure,1)
+cv2.imshow("h_erode",h_dilate_img)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
--- 图片处理/轮廓识别.py
+++ 图片处理/轮廓识别.py
@@ -1,11 +1,13 @@
 # -*- coding: UTF-8 -*-
 import cv2
 import os
+import numpy as np
+
 
 os.chdir(os.path.dirname(__file__))
 
  
-img = cv2.imread('test.png')
+img = cv2.imread('contour.png')
 
 #图片先转成灰度的
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
@@ -24,10 +26,13 @@ ret, binary = cv2.threshold(gray,127,255,cv2.THRESH_BINARY)
 所以读取的图像要先转成灰度的，再转成二值图
 这个函数实际上返回了三个值
 第一个，它返回了你所处理的图像
-第二个，返回我们要找的，轮廓的点集
+第二个，返回我们要找的轮廓集list，list元素为轮廓点集坐标构成的矩阵
 第三个，各层轮廓的索引'''
 binary,contours,hierarchy= cv2.findContours(binary,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)  
-
+print(type(contours),len(contours))#共几个轮廓
+print(type(contours[0]),len(contours[0]))#第一个轮廓的点集个数
+print(contours[0])#第一个轮廓的坐标集
+print(cv2.contourArea(contours[0]))#第一个轮廓的面积
 
 '''第一个参数是指明在哪幅图像上绘制轮廓；
 第二个参数是轮廓本身，在Python中是一个list。
@@ -36,6 +41,15 @@ binary,contours,hierarchy= cv2.findContours(binary,cv2.RETR_TREE,cv2.CHAIN_APPRO
 第五个参数表示轮廓线的宽度，如果是-1，则为填充模式'''
 cv2.drawContours(img,contours,-1,(0,0,255),2)  
 
-  
-cv2.imshow("img", img)  
+cv2.imshow("img", img)
+
+#cv2.contourArea计算轮廓面积,返回轮廓内像素点的个数，此处将轮廓集按面积排序
+c = sorted(contours, key=cv2.contourArea, reverse=True)[0]
+#cv2.minAreaRect主要求得包含点集最小面积的矩形，这个矩形是可以有偏转角度的，可以与图像的边界不平行。
+rect = cv2.minAreaRect(c)
+print(type(rect),rect)
+box = np.int0(cv2.boxPoints(rect))
+cv2.drawContours(img, [box], -1, (0, 255, 0), 3)
+cv2.imshow("largest shape", img)
+
 cv2.waitKey(0)  
