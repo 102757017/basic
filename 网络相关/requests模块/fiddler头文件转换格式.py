acb19f3b9aa465e79b359a0c0bdd6125646e5da4
102757017 <sdf63fg@yeah.net>
2017-10-08 20:39:00 +0800

    PC

--- 网络相关/requests模块/fiddler头文件转换格式.py
+++ 网络相关/requests模块/fiddler头文件转换格式.py
@@ -5,15 +5,17 @@ import pprint
 #从fiddler内复制header到此处
 headers='''Host: www.yv55.com
 Connection: keep-alive
-Content-Length: 0
+Content-Length: 52
 Accept: application/json, text/javascript, */*; q=0.01
 Origin: http://www.yv55.com
 X-Requested-With: XMLHttpRequest
 User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36
+Content-Type: application/x-www-form-urlencoded; charset=UTF-8
 Referer: http://www.yv55.com/PersonalCenter
 Accept-Encoding: gzip, deflate
 Accept-Language: zh-CN,zh;q=0.8
-Cookie: safedog-flow-item=; acw_tc=AQAAAGgmjiZEagEANsUQG7ADO+hDMTt4; ASP.NET_SessionId=0drlfv54gcfu5m5fimroviin; login=117309,2017-10-08 14:41:10; dfw=54awjO1NA5wOthKCv8LDA/ycqv9/p6BiLdLiiVyL0QsPL13pI4bMh/anxSPIxR5Rtg7lU9Y/qJv70RNCULN64CtfREL+Vmp3dQkGeYe68D7ART+6HYIfhPJPNmRUZ1ctBswHBuU5eeEr6AmgPNeigFlJYy3rIwmqoQTi0rtWvzNii/kwFYxoN5NHmzPg3Mbx+f0VLjh23vB28OaenPl3i2N24gPFJeboZ1Zl6J6h4XQDF36jaZVZCxM4tcdgPqnsLKVEhjIjzMhWv3F2Q6OEGaVnT2LHg7ONNie1sKWitiiO8apQeHfh2/e3NnLlL3D+zFwpIOw15dberIYrzNuMUZTSP12AxCKSFOb3+OKwe4s4MWvkW3iBL2Av+iJk98iKeqSnOq83QWPv3NcJVBAnRHnvLTBTfrYm3+RlWbuYzV87vhgrqM8ovcd0iOCbY/PAE/horQvBrL05hHHkTva/RbAqTZ4GpX0c+C23xJg7cBHlR05Q7AOwCFKR6pY7cDUx955wZqIJWYwjkYzJjTGSXSLaIkd3CcPddJzrJXSNrw9Jp//odnmCYYo001inya/emyEwPZyC2snBfw4YQDiv9P5YPKHN8w5376wBsNRyuM5j/n9DdxHI/TuXnKfOqywIG2T2nJwCP1lwmZTIVCynt2Y7TaUS4TImK8wStHvWLXjH+3STZ4xZz1nYy0IafP4TtvVBrhe7rn7kz5Crv3mUBtESwuNzxo03xfhKNJyGTmLDvgubLMmLnKGtf4060767a451bc2b391b7568640b83dc0; si117309=1'''
+Cookie: safedog-flow-item=; acw_tc=AQAAAEiOGmGTNA4ANsUQG4zEXm/RH0KN; ASP.NET_SessionId=ey5fss5lxdeo1roptysc2kpu; login=117309,2017-10-08 20:15:18; si117309=1; dfw=54awjO1NA5wOthKCv8LDA/ycqv9/p6BiLdLiiVyL0QsPL13pI4bMh/anxSPIxR5Rtg7lU9Y/qJv70RNCULN64CtfREL+Vmp3dQkGeYe68D7ART+6HYIfhPJPNmRUZ1ctBswHBuU5eeEr6AmgPNeigFlJYy3rIwmqoQTi0rtWvzNii/kwFYxoN5NHmzPg3Mbx+f0VLjh23vB28OaenPl3i2N24gPFJeboZ1Zl6J6h4XQDF36jaZVZCxM4tcdgPqnsLKVEhjIjzMhWv3F2Q6OEGaVnT2LHg7ONNie1sKWitiiO8apQeHfh2/e3NnLlL3D+gzsEVxSg33G1yusP1MQyiLxcCxOJVpi9iQtoIY5HqUNETFNkzu6o46WplMqVkQPv1Y4aPV3LnY0TeIdz8BB1E/+TWUsU6GSPE9h/snVF4K2Kfiy/Scwd9PfC5m6QeDpGkKtydArrj6+HO0j+rh0dywQdaa6xSN927MkDzFbUPg2ZXf4mqUiKXjngiKaEDHXLRqUoYnedNPCfIlNME7ZInAn+2f/dr7tBCPtnnOQWPYZDeUJ7QW8JQIRS2TrOXTp5fz0M/D7oabiZvw4HYBLUMO6SaERq3DIot2Kfdw3uSVsUPolHKGvvRGTRBibLM4mAqTlBGt80xT/g9LL1P1L0C+xvEfmnw8E405Vt5virQw2jCbWt1tccnbYATX5WyxuDElsykTNHqsv1RI2KJKEZUTWwak/pTEF7xfhKNJyGTmLDvgubLMmLnKGtf5ff8abcf1926c3b90c39bd0b639f17ae'''
+
 
 
 #pprint.pprint(headers)
--- 自动投注.py
+++ 自动投注.py
@@ -29,12 +29,12 @@ headers2={'Accept': 'application/json, text/javascript, */*; q=0.01',
  'Accept-Encoding': 'gzip, deflate',
  'Accept-Language': 'zh-CN,zh;q=0.8',
  'Connection': 'keep-alive',
- 'Content-Length': '0',
- 'Cookie': 'safedog-flow-item=; acw_tc=AQAAAGgmjiZEagEANsUQG7ADO+hDMTt4; '
-           'ASP.NET_SessionId=0drlfv54gcfu5m5fimroviin; '
-           'login=117309,2017-10-08 14:41:10; '
-           'dfw=54awjO1NA5wOthKCv8LDA/ycqv9/p6BiLdLiiVyL0QsPL13pI4bMh/anxSPIxR5Rtg7lU9Y/qJv70RNCULN64CtfREL+Vmp3dQkGeYe68D7ART+6HYIfhPJPNmRUZ1ctBswHBuU5eeEr6AmgPNeigFlJYy3rIwmqoQTi0rtWvzNii/kwFYxoN5NHmzPg3Mbx+f0VLjh23vB28OaenPl3i2N24gPFJeboZ1Zl6J6h4XQDF36jaZVZCxM4tcdgPqnsLKVEhjIjzMhWv3F2Q6OEGaVnT2LHg7ONNie1sKWitiiO8apQeHfh2/e3NnLlL3D+zFwpIOw15dberIYrzNuMUZTSP12AxCKSFOb3+OKwe4s4MWvkW3iBL2Av+iJk98iKeqSnOq83QWPv3NcJVBAnRHnvLTBTfrYm3+RlWbuYzV87vhgrqM8ovcd0iOCbY/PAE/horQvBrL05hHHkTva/RbAqTZ4GpX0c+C23xJg7cBHlR05Q7AOwCFKR6pY7cDUx955wZqIJWYwjkYzJjTGSXSLaIkd3CcPddJzrJXSNrw9Jp//odnmCYYo001inya/emyEwPZyC2snBfw4YQDiv9P5YPKHN8w5376wBsNRyuM5j/n9DdxHI/TuXnKfOqywIG2T2nJwCP1lwmZTIVCynt2Y7TaUS4TImK8wStHvWLXjH+3STZ4xZz1nYy0IafP4TtvVBrhe7rn7kz5Crv3mUBtESwuNzxo03xfhKNJyGTmLDvgubLMmLnKGtf4060767a451bc2b391b7568640b83dc0; '
-           'si117309=1',
+ 'Content-Length': '52',
+ 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+ 'Cookie': 'safedog-flow-item=; acw_tc=AQAAAEiOGmGTNA4ANsUQG4zEXm/RH0KN; '
+           'ASP.NET_SessionId=ey5fss5lxdeo1roptysc2kpu; '
+           'login=117309,2017-10-08 20:15:18; si117309=1; '
+           'dfw=54awjO1NA5wOthKCv8LDA/ycqv9/p6BiLdLiiVyL0QsPL13pI4bMh/anxSPIxR5Rtg7lU9Y/qJv70RNCULN64CtfREL+Vmp3dQkGeYe68D7ART+6HYIfhPJPNmRUZ1ctBswHBuU5eeEr6AmgPNeigFlJYy3rIwmqoQTi0rtWvzNii/kwFYxoN5NHmzPg3Mbx+f0VLjh23vB28OaenPl3i2N24gPFJeboZ1Zl6J6h4XQDF36jaZVZCxM4tcdgPqnsLKVEhjIjzMhWv3F2Q6OEGaVnT2LHg7ONNie1sKWitiiO8apQeHfh2/e3NnLlL3D+gzsEVxSg33G1yusP1MQyiLxcCxOJVpi9iQtoIY5HqUNETFNkzu6o46WplMqVkQPv1Y4aPV3LnY0TeIdz8BB1E/+TWUsU6GSPE9h/snVF4K2Kfiy/Scwd9PfC5m6QeDpGkKtydArrj6+HO0j+rh0dywQdaa6xSN927MkDzFbUPg2ZXf4mqUiKXjngiKaEDHXLRqUoYnedNPCfIlNME7ZInAn+2f/dr7tBCPtnnOQWPYZDeUJ7QW8JQIRS2TrOXTp5fz0M/D7oabiZvw4HYBLUMO6SaERq3DIot2Kfdw3uSVsUPolHKGvvRGTRBibLM4mAqTlBGt80xT/g9LL1P1L0C+xvEfmnw8E405Vt5virQw2jCbWt1tccnbYATX5WyxuDElsykTNHqsv1RI2KJKEZUTWwak/pTEF7xfhKNJyGTmLDvgubLMmLnKGtf5ff8abcf1926c3b90c39bd0b639f17ae',
  'Host': 'www.yv55.com',
  'Origin': 'http://www.yv55.com',
  'Referer': 'http://www.yv55.com/PersonalCenter',
@@ -46,7 +46,7 @@ headers2={'Accept': 'application/json, text/javascript, */*; q=0.01',
 
 url2="http://www.yv55.com/uc/GetAllBet?0.4610653719152149"
 url3="http://www.yv55.com/uc/SubmitBet?0.7372482543845444"
-post_body="issueNum=201710070782&hongtl=100&DFW=0&huitl=0&myy=0"
+post_body="issueNum=201710081215&hongtl=100&DFW=0&huitl=0&myy=0"
 
 
 def checktime():
@@ -57,7 +57,8 @@ def checktime():
         print(r.text)
     a=result['list']
     b=result['nTime']
-    return b
+    a4=a['IssueNum']
+    return b,a4
 
 def getbat():
     r=requests.post(url2,headers=headers)
@@ -84,11 +85,17 @@ def bat():
     print(d)
         
 b=checktime()
+IssueNum=str(int(b[1])+1)
+post_body="issueNum="+IssueNum+"&hongtl=100&DFW=0&huitl=0&myy=0"
+
 while 1:
-    if b>39000:
-        dt=(b-39000)/1000
+    if b[0]>39000:
+        dt=(b[0]-39000)/1000
+        IssueNum=str(int(b[1])+1)
+        post_body="issueNum="+IssueNum+"&hongtl=100&DFW=0&huitl=0&myy=0"        
     else:
-        dt=(b+21000)/1000
+        dt=(b[0]+21000)/1000
+        post_body="issueNum="+b[1]+"&hongtl=100&DFW=0&huitl=0&myy=0"
     print("wait:",dt)
     time.sleep(dt)
     try:
@@ -96,9 +103,9 @@ while 1:
         print('筹码差额',c/100000000)
         if c>-130000000 and c<-75000000:
             bat()
+        b=checktime()
     except BaseException as e:
         print("产生了错误",e)
         pass
     finally:
-        b=checktime()
         print(b)
