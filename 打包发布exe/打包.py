c25e90997e0e59847e5eb8c50a937dc75e953fad
102757017 <sdf63fg@yeah.net>
2018-06-24 23:22:51 +0800

    180624

--- 打包发布exe/打包.py
+++ 打包发布exe/打包.py
@@ -1,15 +1,19 @@
 # -*- coding: UTF-8 -*-
 import os
 import sys
+import subprocess
 
-os.chdir(sys.path[0])
-#a="\""+ sys.prefix + "\Scripts\pyinstaller.exe\"" +" hello.py"
-a="\""+ sys.prefix + "\python.exe\""  + "\""+ sys.prefix + "\Scripts\pyinstaller.exe\"" +" hello.py"
+
+#在64位系统下打包的程序只能在64位系统上运行
+
+
+sys.path.append(os.path.dirname(__file__))
+a="\""+ sys.prefix + "\python.exe\" "  + "\""+ sys.prefix + "\Scripts\pyinstaller.exe\" " +os.path.dirname(__file__)+"\hello.py"+" --distpath="+os.path.dirname(__file__)
 
 #生成command命令,指定安装源为阿里云
 print("command命令是："+ a)
-os.system(a) #执行command
 
-'''
-"G:\Program Files\WinPython-64bit-3.6.1.0Qt5\python-3.6.1.amd64\python.exe" "G:\Program Files\WinPython-64bit-3.6.1.0Qt5\python-3.6.1.amd64\Scripts\pyinstaller.exe" "H:\学习资料\编程学习\pathon\基础操作\basic\打包发布exe\hello.py"
-'''
+pipe = subprocess.Popen(a, shell=True, stdout=subprocess.PIPE,stderr=subprocess.PIPE, universal_newlines=True).stdout  
+print(pipe.read())  
+
+
