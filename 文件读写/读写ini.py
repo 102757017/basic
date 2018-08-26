f5c5e6ecd87efae215916c13d8efa18f61b1c281
102757017 <sdf63fg@yeah.net>
2018-08-26 22:29:01 +0800

    添加判断配置文件是否存在
    Signed-off-by: 102757017 <sdf63fg@yeah.net>

--- 文件读写/读写ini.py
+++ 文件读写/读写ini.py
@@ -1,20 +1,25 @@
 # -*- coding: UTF-8 -*-
 import configparser
+import os
 
 
+    
+#在传递键值对数据时，会将键名 全部转化为小写
 conf = configparser.ConfigParser()
-conf.read("seting.ini")
+if os.path.isfile("seting.ini"):
+    conf.read("seting.ini")
+    level  = conf.get("logging", "level")
+    host=conf.get("mysql","host")
+    port=conf.get("mysql","port")
+else:
+    conf.add_section('logging')
+    conf.set('logging', 'level', '20')
+    conf.add_section('mysql')
+    conf.set('mysql', 'host', '127.0.0.1')
+    conf.set('mysql', 'port', '80')
+    conf.write(open('seting.ini', 'w'))
 
-sections = conf.sections()
-print('获取配置文件所有的section', sections)
 
-options = conf.options('mysql')
-print('获取指定section下所有option', options)
+port=conf.get("mysql","port")
+print('获取指定的section下的option', type(port), port)
 
-
-items = conf.items('mysql')
-print('获取指定section下所有的键值对', items)
-
-
-value = conf.get('mysql', 'host')
-print('获取指定的section下的option', type(value), value)
