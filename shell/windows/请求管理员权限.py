ab55de82a1f70ea7e7cd36c7bf5669d31b0e9f67
102757017 <sdf63fg@yeah.net>
2018-09-14 22:34:51 +0800

    请求管理员选项会创建一个新的进程，原进程还是没有管理员权限
    Signed-off-by: 102757017 <sdf63fg@yeah.net>

--- shell/windows/请求管理员权限.py
+++ shell/windows/请求管理员权限.py
@@ -10,7 +10,10 @@ def is_admin():
 
 if is_admin():
     # 在此处填入需要执行的代码
-    pass
+    print("获取到了管理员权限")
+    a=input("")
+    
 else:
     # Re-run the program with admin rights
-    ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, "", None, 1)
+    ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, __file__, None, 1)
+
