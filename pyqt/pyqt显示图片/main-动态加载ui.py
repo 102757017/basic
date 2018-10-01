915452ff6a88e1be793349e493bbc64b7013e0e5
102757017 <sdf63fg@yeah.net>
2018-10-01 21:18:30 +0800

    鼠标事件代码重构
    Signed-off-by: 102757017 <sdf63fg@yeah.net>

--- pyqt/pyqt显示图片/main-动态加载ui.py
+++ pyqt/pyqt显示图片/main-动态加载ui.py
@@ -4,6 +4,7 @@ from PyQt5.uic import loadUi
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
+from types import MethodType
 
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -12,10 +13,85 @@ class MainWindow(QtWidgets.QMainWindow):
 		loadUi('form1.ui', self)
 
 		#建立画板
-		self.scene = QGraphicsScene(self)
-		self.graphicsView.setScene(self.scene)
-		self.draw=""
-		self.flag=False
+		self.graphicsView.scene = QGraphicsScene(self)
+		self.graphicsView.setScene(self.graphicsView.scene)
+		self.graphicsView.draw=""
+		self.graphicsView.flag=False
+
+
+		# 重写鼠标按下事件
+		def mousePressEvent(self, e):
+			print("mousePressEvent", e.button())
+			self.flag = True
+			self.x0 = e.x()
+			self.y0 = e.y()
+			# 主窗口中如果不使用grabMouse()，将只能响应mousepress事件而不能响应mouserelease事件以及mousemove事件
+			#self.grabMouse()
+
+			# 添加line的子函数
+			if self.draw == "Line":
+				self.line = QGraphicsLineItem(self.x0, self.y0, self.x0, self.y0)
+				self.scene.addItem(self.line)
+
+			# 添加Rect的子函数
+			if self.draw == "Rect":
+				# 前两个参数为坐标，后两个参数为长宽
+				self.rect = QGraphicsRectItem(self.x0, self.y0, 0, 0)
+				# 设置为可选择，可拖拽
+				self.rect.setFlags(QGraphicsItem.ItemIsSelectable | QGraphicsItem.ItemIsMovable)
+				self.scene.addItem(self.rect)
+
+			# 添加椭圆的子函数
+			if self.draw == "Ellipse":
+				# 前两个参数为坐标，后两个参数为长宽
+				self.ellipse = QGraphicsEllipseItem(self.x0, self.y0, 0, 0)
+				# 设置为可选择，可拖拽
+				self.ellipse.setFlags(QGraphicsItem.ItemIsSelectable | QGraphicsItem.ItemIsMovable)
+				self.scene.addItem(self.ellipse)
+
+			# 添加文字的子函数
+			if self.draw == "Text":
+				self.text = QGraphicsTextItem("hello world")
+				self.text.setPos(QPoint(self.x0, self.y0))
+				self.scene.addItem(self.text)
+
+			self.scene.update()
+
+		# 定义鼠标移动事件
+		def mouseMoveEvent(self, e):
+			print("mouseMoveEvent：", e.pos())
+			self.x2 = e.x()
+			self.y2 = e.y()
+
+			# 修改矩形大小
+			if hasattr(self, "rect") and self.draw == "Rect" and self.flag == True:
+				self.rect.setRect(self.x0, self.y0, self.x2 - self.x0, self.y2 - self.y0)
+
+			# 修改直线终点
+			if hasattr(self, "line") and self.draw == "Line" and self.flag == True:
+				self.line.setLine(self.x0, self.y0, self.x2, self.y2)
+
+			# 修改椭圆大小
+			if hasattr(self, "ellipse") and self.draw == "Ellipse" and self.flag == True:
+				self.ellipse.setRect(self.x0, self.y0, self.x2 - self.x0, self.y2 - self.y0)
+
+			self.scene.update()
+
+		# 定义鼠标释放事件
+		def mouseReleaseEvent(self, e):
+			print("mouseReleaseEvent", e.button())
+			self.flag = False
+			self.x1 = e.x()
+			self.y1 = e.y()
+			print("鼠标释放坐标：", e.x(), e.y())
+			#self.releaseMouse()
+
+
+
+		#使用MethodType将函数转换为方法，再绑定到实例上去
+		self.graphicsView.mousePressEvent=MethodType(mousePressEvent,self.graphicsView)
+		self.graphicsView.mouseMoveEvent = MethodType(mouseMoveEvent, self.graphicsView)
+		self.graphicsView.mouseReleaseEvent = MethodType(mouseReleaseEvent, self.graphicsView)
 
 
 		# 将按钮点击事件和槽函数绑定
@@ -35,96 +111,26 @@ class MainWindow(QtWidgets.QMainWindow):
 		pixmap=QPixmap()
 		pixmap.load(fileName)
 		item=QtWidgets.QGraphicsPixmapItem(pixmap)
-		self.scene.addItem(item)
+		self.graphicsView.scene.addItem(item)
 
 	def addText(self):
-		self.draw="Text"
+		self.graphicsView.draw="Text"
 
 	def addRect(self):
-		self.draw = "Rect"
+		self.graphicsView.draw = "Rect"
 
 	def addLine(self):
-		self.draw = "Line"
+		self.graphicsView.draw = "Line"
 
 	def addEllipse(self):
-		self.draw = "Ellipse"
+		self.graphicsView.draw = "Ellipse"
 
 	def deletAll(self):
-		item_list=self.scene.items()
+		item_list=self.graphicsView.scene.items()
 		for item in item_list:
-			self.scene.removeItem(item)
+			self.graphicsView.scene.removeItem(item)
+		self.graphicsView.scene.update()
 		self.draw = ""
-
-
-	#定义鼠标按下事件
-	def mousePressEvent(self,e):
-		print("mousePressEvent",e.button())
-		# 获取的鼠标点击位置是相对于主窗口的坐标，将其转化为graphicsView内的坐标
-		pos = self.graphicsView.mapFrom(self, e.pos())
-		self.flag=True
-		self.x0=pos.x()
-		self.y0=pos.y()
-		#如果不使用grabMouse()，将只能响应mousepress事件而不能响应mouserelease事件以及mousemove事件
-		self.grabMouse()
-
-		#添加line的子函数
-		if self.draw=="Line":
-			self.line = QGraphicsLineItem(self.x0, self.y0, self.x0, self.y0)
-			self.scene.addItem(self.line)
-
-		#添加Rect的子函数
-		if self.draw=="Rect":
-                        #前两个参数为坐标，后两个参数为长宽
-			self.rect = QGraphicsRectItem(self.x0, self.y0, 0, 0)
-			# 设置为可选择，可拖拽
-			self.rect.setFlags(QGraphicsItem.ItemIsSelectable | QGraphicsItem.ItemIsMovable)
-			self.scene.addItem(self.rect)
-
-		#添加椭圆的子函数
-		if self.draw=="Ellipse":
-                        #前两个参数为坐标，后两个参数为长宽
-			self.ellipse = QGraphicsEllipseItem(self.x0, self.y0, 0, 0)
-			#设置为可选择，可拖拽
-			self.ellipse.setFlags(QGraphicsItem.ItemIsSelectable | QGraphicsItem.ItemIsMovable)
-			self.scene.addItem(self.ellipse)
-
-
-	#定义鼠标移动事件
-	def mouseMoveEvent(self,e):
-		# 获取的鼠标点击位置是相对于主窗口的坐标，将其转化为graphicsView内的坐标
-		pos = self.graphicsView.mapFrom(self, e.pos())
-		print("mouseMoveEvent：",pos)
-		self.x2=pos.x()
-		self.y2=pos.y()
-
-		#修改矩形大小
-		if hasattr(self,"rect") and self.draw=="Rect":
-			self.rect.setRect(self.x0,self.y0,self.x2-self.x0,self.y2-self.y0)
-
-		#修改直线终点
-		if hasattr(self,"line") and self.draw=="Line":
-			self.line.setLine(self.x0,self.y0,self.x2,self.y2)
-
-		#修改椭圆大小
-		if hasattr(self,"ellipse") and self.draw=="Ellipse":
-			self.ellipse.setRect(self.x0,self.y0,self.x2-self.x0,self.y2-self.y0)
-
-	#定义鼠标释放事件
-	def mouseReleaseEvent(self,e):
-		print("mouseReleaseEvent",e.button())
-		# 获取的鼠标点击位置是相对于主窗口的坐标，将其转化为graphicsView内的坐标
-		pos = self.graphicsView.mapFrom(self, e.pos())
-		self.flag=False
-		self.x1=pos.x()
-		self.y1=pos.y()
-		print("graphicsView坐标：",pos.x(),pos.y())
-		self.releaseMouse()
-
-		#添加文字的子函数
-		if self.draw=="Text":
-			text=QGraphicsTextItem("hello world")
-			text.setPos(QPointF(self.x0,self.y0))
-			self.scene.addItem(text)
 
 
 
