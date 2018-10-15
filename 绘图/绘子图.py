afb3d0fd555ad9a323e8a0b700c27c90f707ca45
102757017 <sdf63fg@yeah.net>
2018-10-15 20:21:14 +0800

    添加各个子图的标题
    Signed-off-by: 102757017 <sdf63fg@yeah.net>

--- 绘图/绘子图.py
+++ 绘图/绘子图.py
@@ -3,15 +3,27 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+# 生成画布
+fig=plt.figure()
 
-#画子图，将画布分割成1行1列，图像画在从左到右从上到下的第1块
-plt.subplot(311)
-plt.plot([1,2,3,4],label="sample1")
+#生成子图，将画布分割成1行1列，图像画在从左到右从上到下的第1块
+ax1=fig.add_subplot(311)
+#设置子图横坐标范围
+ax1.set_xlim(0, 5)
+#设置子图纵坐标范围
+ax1.set_ylim(0, 5)
+ax1.plot([1,2,3,4])
+ax1.set_title("sample1")
 
-plt.subplot(312)
-plt.plot([0.5, 2, 3, 4], [1, 4, 5, 10],label="sample2")
+#生成子图
+ax2=fig.add_subplot(312)
+ax2.plot([0.5, 2, 3, 4], [1, 4, 5, 10])
+ax2.set_title("sample2")
 
-plt.subplot(313)
-plt.plot([0,2,3,4], [1,6,9,16], 'ro',label="sample3")
+#生成子图
+ax3=fig.add_subplot(313)
+ax3.plot([0,2,3,4], [1,6,9,16], 'ro')
+ax3.set_title("sample3")
+
 
 plt.show()
