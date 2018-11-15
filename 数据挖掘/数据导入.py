943f5cae7d197f3b6e8934963ee7dd0bc7382524
102757017 <sdf63fg@yeah.net>
2018-11-15 23:55:51 +0800

    矩阵的连接
    Signed-off-by: 102757017 <sdf63fg@yeah.net>

--- 数据挖掘/数据导入.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: UTF-8 -*-
-import requests
-url = 'http://aima.cs.berkeley.edu/data/iris.csv'
-u=requests.get(url)
-localFile = open('iris.csv','w')
-localFile.write(u.text)
-localFile.close()
