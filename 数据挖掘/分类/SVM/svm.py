7b24cc5840b88d88d34ccb157ba7d3580c4181ed
102757017 <sdf63fg@yeah.net>
2018-11-20 22:33:23 +0800

    分类算法

---
+++ 数据挖掘/分类/SVM/svm.py
@@ -0,0 +1,28 @@
+# -*- coding: UTF-8 -*-
+from sklearn.datasets import load_iris
+from sklearn import svm
+import sys
+import os
+import numpy as np
+
+#给系统添加环境变量，修改的环境变量是临时改变的，当程序停止时修改的环境变量失效（系统变量不会改变）
+os.environ["Path"] += os.pathsep + r"G:\Program Files\WinPython-64bit-3.6.1.0Qt5\graphviz\bin"
+
+iris =load_iris()
+print(iris["data"])
+print(iris["target"])
+print(iris["target_names"])
+print(iris["data"].shape)
+
+
+#创建svm模型
+clf = svm.SVC()
+#训练模型
+clf.fit(iris["data"],iris["target"])
+
+#自己随便写的一个样本数据
+x_new=[5,3,1,0.1]
+#将数据转换为一行的矩阵
+x_new = np.array(x_new).reshape(1, -1)
+#预测
+print("x_new的预测结果：",clf.predict(x_new))
