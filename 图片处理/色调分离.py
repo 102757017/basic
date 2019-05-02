e6fbd5074dbe14ce7f77ed84d536a939979dfdbe
102757017 <sdf63fg@yeah.net>
2019-05-02 23:02:58 +0800

    photoshop内色调分离的算法

--- 图片处理/色调分离.py
+++ 图片处理/色调分离.py
@@ -8,22 +8,20 @@ import time
 os.chdir(os.path.dirname(__file__))
 
 def sdfl(array,n):
+    obj=np.zeros_like(array, dtype=np.uint8)
     for i in range(n):
         value=int(255/(n-1))*i
         low=255/n*i
         high=255/n*(i+1)
         area=np.where((array>low)&(array<high))
-        array[area]=value
-    return array
+        obj[area]=value
+    return obj
 
-#由于opencv不支持读取中文路径，用以下方法代替cv2.imread
-img = cv2.imdecode(np.fromfile('aaa.jpg', dtype=np.uint8), 1)
-
-
-
-
-target = sdfl(img,2)
-cv2.imshow('image',target)
-cv2.imwrite('binary.png',target)
-
+if __name__=="__main__":
+    os.chdir(os.path.dirname(__file__))
+    #由于opencv不支持读取中文路径，用以下方法代替cv2.imread
+    img = cv2.imdecode(np.fromfile('get-captcha.png', dtype=np.uint8), 1)
+    target = sdfl(img,2)
+    cv2.imshow('image',target)
+    cv2.imwrite('re_color.png',target)
 
