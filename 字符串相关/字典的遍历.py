cf80ce57ba7efde3ac4172b3f2ecdd6588c4ffa3
102757017 <sdf63fg@yeah.net>
2019-12-08 19:05:43 +0800

    使用递归对多重dict进行变量
    Signed-off-by: 102757017 <sdf63fg@yeah.net>

--- 字符串相关/字典的遍历.py
+++ 字符串相关/字典的遍历.py
@@ -24,3 +24,21 @@ print('\n')
 for (x,y) in d.items():
     print(x,y)
 print('\n')
+
+
+d={'code': {'code': 0, 'message': 'SUCCESS'}, 'data': {'dynStock': {'holdQuantity': 0, 'sellableQuantity': 8, 'sku': {';1627207:28320;': {'holdQuantity': 0, 'oversold': False, 'sellableQuantity': 8, 'stock': 8}}, 'stock': 8, 'stockType': 'normal'}}}
+#多重dict的遍历,print所有根节点的值
+def list_all_dict(dict_a):
+    #使用isinstance检测数据类型
+    if isinstance(dict_a,dict):
+        for (x,y) in dict_a.items():
+            if isinstance(y,dict):
+                pass
+            else:
+                #print根节点的key.value
+                print(x,y)
+                
+            #自我调用实现无限遍历
+            list_all_dict(y)
+
+list_all_dict(d)
