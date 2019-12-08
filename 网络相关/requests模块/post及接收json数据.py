9046fd4677df297a10bc59e4bd349c3d7e3049ff
102757017 <sdf63fg@yeah.net>
2019-12-08 19:11:39 +0800

    解析requests接收到的json数据
    Signed-off-by: 102757017 <sdf63fg@yeah.net>

--- 网络相关/requests模块/post及接收json数据.py
+++ 网络相关/requests模块/post及接收json数据.py
@@ -1,6 +1,7 @@
 # -*- coding: UTF-8 -*-
 import requests
 import json
+import pprint
 
 url = 'http://httpbin.org/post'
 
@@ -8,3 +9,7 @@ url = 'http://httpbin.org/post'
 data = "investCheckRequestParam=%s" %  json.dumps({"productId":"181333172208832","investSource":"0","investAmount":"18026.04","bizChannel":"secmkt","tradingType":"35"})
 r = requests.post(url, data=data)
 print(r.text)
+
+#使用r.json()获取到的是dict格式，使用r.text获取到的是string格式
+d=r.json()
+pprint.pprint(d)
