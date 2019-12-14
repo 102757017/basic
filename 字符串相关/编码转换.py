a6208f8f522f12d2c9eea5f5e965fb4273d31ed8
102757017 <sdf63fg@yeah.net>
2019-12-14 20:04:14 +0800

    增加html解码
    Signed-off-by: 102757017 <sdf63fg@yeah.net>

--- 字符串相关/编码转换.py
+++ 字符串相关/编码转换.py
@@ -4,6 +4,7 @@ import chardet
 import urllib.parse
 import sys
 import locale
+import html
 
 print("当前系统shell标准输出的编码",sys.stdout.encoding)
 
@@ -27,35 +28,30 @@ print(b)
 print("\n")
 
 
-b='中文'
-b=b.encode('utf-8')
-print("将'中文'转换为utf-8：")
-print(b)
+b="中文"
+print("将'中文'转换为utf-8编码：",b.encode('utf-8'))
+print("将'中文'转换为GBK编码：",b.encode('GBK'))
+print("将'中文'转换为GB2312编码：",b.encode('GB2312'))
+print("将'中文'转换为GB18030编码：",b.encode('GB18030'))
 print(r"\x：是16进制的意思，\d：十进制；\o：八进制；后边跟两位，则表示单字节编码")
-print(chardet.detect(b))
+print("\n")
+print("将'中文'转换为Unicode编码：",b.encode('unicode_escape'))
 print("\n")
 
 
-print("将utf-8码的'中文'再转换为unicode码：")
-b=b.decode('utf-8')
-print(b)
+
+
+#解码
+s='\\u653e\\u5f03\\u4fee\\u6539'
+print("s:",s)
+s2=s.encode('utf-8')
+print("首先将string转换为bytes：",s2)
+s3=s2.decode('unicode_escape')
+print("然后将bytes进行解码：",s3)
+
+
+
 print("\n")
-
-
-b='中文'
-print("将'中文'转换为GBK编码：")
-b=b.encode('gbk')
-print(b)
-print(chardet.detect(b))
-print("\n")
-
-
-print("将GBK编码的'中文'再转换为unicode码：")
-b=b.decode('gbk')
-print(b)
-print("\n")
-
-
 print("屏蔽特殊的字符、比如如果url里面的空格或中文！url里面是不允许出现空格的")
 b=urllib.parse.quote(b)
 print(b)
@@ -66,3 +62,16 @@ b=urllib.parse.unquote(b)
 print(b)
 print("\n")
 
+
+'''
+html中也存在转义符号，以下是一些例子
+字符		转义字符
+"		&quot;
+&		&amp;
+<		&lt;
+>		&gt;
+'''
+url="htttp://taobao.com?id=123&ampuser=456"
+print("原始url",url)
+print("反转义",html.unescape(url))
+html.unescape('a=1&amp;b=2')
