742b7e8db69bb0c2c3e300602bf9a7941b246f07
102757017 <sdf63fg@yeah.net>
2019-12-30 23:57:13 +0800

    追加了一些转换方法
    Signed-off-by: 102757017 <sdf63fg@yeah.net>

--- 字符串相关/进制转换.py
+++ 字符串相关/进制转换.py
@@ -10,26 +10,31 @@ print("显示十进制对应的ascii码图形：%c" % n)
 print("显示字符串：%s" % n)
 print("\n")
 
+
 # int本身不需要进制转换，print时可以打印出不同进制
-# 使用Python内置函数：bin()、hex()可实现int到str的进制转换
+print("使用Python内置函数：bin()、hex()可实现int到str的进制转换")
 print("10进制int转换为2进制字符串", bin(n), type(bin(n)))
-
-
 print("10进制int转为16进制字符串", hex(n), type(bin(n)))
-
-
 print("2进制int转换到16进制字符串", hex(0b10101011), type(hex(0b10101011)))
-
-
 print("16进制int转换到2进制字符串", bin(0xab), type(bin(0xab)))
 
 
-# 使用int()可实现str到int的进制转换
+print("使用formate进行格式转换")
+print("10进制int转换为2进制字符串", '{:b}'.format(n))
+print("10进制int转为8进制字符串", '{:o}'.format(n))
+print("10进制int转换到16进制字符串", '{:x}'.format(n))
+print("10进制int用Unicode解码为字符串", '{:c}'.format(n))
+print("\n")
+
+print("将16进制字符用ASCII转换为bytes", '{:x}'.format(n).encode("ascii"))
+print("\n")
+
+
+print("使用int()可实现str到int的进制转换")
 print("2进制字符串转为10进制int", int("10101011", 2))
 print("16进制字符串转为10进制int", int("0xab", 16))
 print("16进制字符串转为10进制int", int("ab", 16))
 print("\n")
-
 
 n = 65535
 print("显示十进制数据：%d" % n)
