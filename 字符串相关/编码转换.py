45547ca1c68432489417c96347b078e3b6b7e3fe
102757017 <sdf63fg@yeah.net>
2019-12-31 15:58:30 +0800

    add注释
    Signed-off-by: 102757017 <sdf63fg@yeah.net>

--- 字符串相关/编码转换.py
+++ 字符串相关/编码转换.py
@@ -6,16 +6,19 @@ import sys
 import locale
 import html
 
-print("当前系统shell标准输出的编码",sys.stdout.encoding)
+print("当前系统shell标准输出的编码", sys.stdout.encoding)
 
 
-print ("文件系统的编码",sys.getfilesystemencoding())
+print("文件系统的编码", sys.getfilesystemencoding())
 
+# 使用encode()将str转换为bytes，bytes就是一串hex数据，主要用于网络传输，同一个str根据编码方式不同可以转换为不同的bytes
+# 使用decode()将bytes转换为str，单纯的bytes是没有意义的一堆二进制数据，只能知道了编码方式才能解析
+# web传输一般用UTF-8编码解码：bytes→str（UTF-8解码）
+# 串口传输一般用ASCII编码解码：bytes→str（ASCII解码）→int
 
-
-b='test str'
-b=b.encode('ascii')
-#含有中文的str无法用ASCII编码，因为中文编码的范围超过了ASCII编码的范围
+b = 'test str'
+b = b.encode('ascii')
+# 含有中文的str无法用ASCII编码，因为中文编码的范围超过了ASCII编码的范围
 print("将'test str'转换为ascii：")
 print(b)
 print(chardet.detect(b))
@@ -23,42 +26,40 @@ print("\n")
 
 
 print("将ascii码的'test str'再转换为unicode码：")
-b=b.decode('ascii')
+b = b.decode('ascii')
 print(b)
 print("\n")
 
 
-b="中文"
-print("将'中文'转换为utf-8编码：",b.encode('utf-8'))
-print("将'中文'转换为GBK编码：",b.encode('GBK'))
-print("将'中文'转换为GB2312编码：",b.encode('GB2312'))
-print("将'中文'转换为GB18030编码：",b.encode('GB18030'))
+b = "中文"
+print("将'中文'转换为utf-8编码：", b.encode('utf-8'))
+print("将'中文'转换为GBK编码：", b.encode('GBK'))
+print("将'中文'转换为GB2312编码：", b.encode('GB2312'))
+print("将'中文'转换为GB18030编码：", b.encode('GB18030'))
+print("将'中文'转换为unicode_escape编码：", b.encode('unicode_escape'))
 print(r"\x：是16进制的意思，\d：十进制；\o：八进制；后边跟两位，则表示单字节编码")
 print("\n")
-print("将'中文'转换为Unicode编码：",b.encode('unicode_escape'))
+print("将'中文'转换为Unicode编码：", b.encode('unicode_escape'))
 print("\n")
 
 
-
-
-#解码
-s='\\u653e\\u5f03\\u4fee\\u6539'
-print("s:",s)
-s2=s.encode('utf-8')
-print("首先将string转换为bytes：",s2)
-s3=s2.decode('unicode_escape')
-print("然后将bytes进行解码：",s3)
-
+# 解码
+s = '\\u653e\\u5f03\\u4fee\\u6539'
+print("s:", s)
+s2 = s.encode('utf-8')
+print("首先将string转换为bytes：", s2)
+s3 = s2.decode('unicode_escape')
+print("然后将bytes进行解码：", s3)
 
 
 print("\n")
 print("屏蔽特殊的字符、比如如果url里面的空格或中文！url里面是不允许出现空格的")
-b=urllib.parse.quote(b)
+b = urllib.parse.quote(b)
 print(b)
 print("\n")
 
 print("url反向转换")
-b=urllib.parse.unquote(b)
+b = urllib.parse.unquote(b)
 print(b)
 print("\n")
 
@@ -71,7 +72,7 @@ html中也存在转义符号，以下是一些例子
 <		&lt;
 >		&gt;
 '''
-url="htttp://taobao.com?id=123&ampuser=456"
-print("原始url",url)
-print("反转义",html.unescape(url))
+url = "htttp://taobao.com?id=123&ampuser=456"
+print("原始url", url)
+print("反转义", html.unescape(url))
 html.unescape('a=1&amp;b=2')
