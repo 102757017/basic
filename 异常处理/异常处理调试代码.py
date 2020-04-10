4864e12f922fcd95359aa94638093ddd3149b2c5
102757017 <sdf63fg@yeah.net>
2020-04-10 23:01:07 +0800

    增加自主抛出异常
    Signed-off-by: 102757017 <sdf63fg@yeah.net>

--- 异常处理/异常处理调试代码.py
+++ 异常处理/异常处理调试代码.py
@@ -7,18 +7,24 @@ try:
 
 #捕获ValueError类的错误
 except ValueError as e:
-    print('错误信息是:', e)
+    print('ValueError错误信息是:', e)
     
 #捕获ZeroDivisionError类的错误
 except ZeroDivisionError as e:
-    print('错误信息是:', e)
+    print('ZeroDivisionError错误信息是:', e)
 
 #捕获所有类的错误，Python所有的错误都是从BaseException类派生的
 except BaseException as e:
-    print('错误信息是:', e)
+    print('BaseException错误信息是:', e)
 
 #不管有没有出错， finally后面的语句是一定会执行的
 finally:
     print('finally后面的语句是一定会执行的')
-               
 
+
+try:         
+   raise ValueError("主动抛出异常，后面的代码不继续执行")
+   print("后面的代码")
+#捕获ValueError类的错误
+except ValueError as e:
+    print('ValueError错误信息是:', e)
