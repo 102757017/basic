480487dba2abc9f71b933fe6d0e6d0a9353dbce7
102757017 <sdf63fg@yeah.net>
2020-06-06 23:13:55 +0800

    使用变量插入数据
    Signed-off-by: 102757017 <sdf63fg@yeah.net>

--- 数据库操作/insert.py
+++ 数据库操作/insert.py
@@ -16,8 +16,11 @@ cursor = conn.cursor()
 
 #自增字段必须插入空值，以下两种方式插入空值的方法是不一样的。
 dt=time.strftime("%m/%d/%Y %H:%M")
-#cursor.execute(r"insert into table1 (链接, 线报,权限,时间) values ('http://5', '吐槽','10','2019-1-22')")
-#cursor.execute("insert into table1 values (?,?,?,?)",('http://5', '吐槽','10',time.strftime("%Y-%m-%d %H:%M")))
+
+#不使用变量
+cursor.execute(r"insert into table1 (链接, 线报,权限,时间) values ('http://5', '吐槽','10','2019-1-22')")
+#使用变量
+cursor.execute(r"insert into table1 (链接, 线报,权限,时间) values ('{}','{}','{}','{}')".format('http://5', '吐槽','10','2019-1-22'))
 
 #防止插入重复数据:先将组合字段设置为唯一键或联合主键，再使用insert or ignore插入数据，忽略已存在数据。若使用insert or replace则可以替换已存在的数据
 cursor.execute(r"insert or ignore into table1 (链接, 线报,权限,时间) values ('http://5', '吐槽','10','2019-1-22')")
