a75f48876af20d89c706761c81c78a98039c3bba
102757017 <sdf63fg@yeah.net>
2020-06-30 15:39:28 +0800

    添加水平线，垂直线
    Signed-off-by: 102757017 <sdf63fg@yeah.net>

--- 绘图/曲线图散点图.py
+++ 绘图/曲线图散点图.py
@@ -5,6 +5,7 @@ import numpy as np
 from time import sleep
 
 
+
 #只提供了一个list，matplotlib就会假设这个序列是Y轴上的取值，并且会自动为你生成X轴上的值
 plt.plot([1,2,3,4],label="sample1")
 
@@ -14,6 +15,9 @@ plt.plot([0.5, 2, 3, 4], [1, 4, 5, 10],label="sample2")
 
 #绘制散点图
 plt.plot([0,2,3,4], [1,6,9,16], 'ro',label="sample3")
+
+plt.vlines(1,ymin=0, ymax=10,colors = "r", linestyles = "dashed")
+plt.hlines(4, xmin=0, xmax=4, colors = "r", linestyles = "dashed")
 
 #绘制标签栏
 #loc参数设置标签的显示位置，'best'为自适应方式
