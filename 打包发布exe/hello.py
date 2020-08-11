155b956738f5732a01d61f3c438492fdee490160
102757017 <sdf63fg@yeah.net>
2020-08-11 19:16:00 +0800

    显示工作目录
    Signed-off-by: 102757017 <sdf63fg@yeah.net>

--- 打包发布exe/hello.py
+++ 打包发布exe/hello.py
@@ -1,2 +1,12 @@
 # -*- coding: UTF-8 -*-
+import sys,os
 print('hello')
+print(__file__)
+
+
+
+if getattr( sys, 'frozen', False ) :
+        bundle_dir=sys._MEIPASS
+else :
+        bundle_dir=os.path.dirname(os.path.abspath(__file__))
+print(bundle_dir)
