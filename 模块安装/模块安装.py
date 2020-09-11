3f4079b69b08fad649e4368b8aaab06ae7cfa43e
102757017 <sdf63fg@yeah.net>
2020-09-11 19:51:53 +0800

    使用管道执行方式
    Signed-off-by: 102757017 <sdf63fg@yeah.net>

--- 模块安装/模块安装.py
+++ 模块安装/模块安装.py
@@ -1,10 +1,19 @@
 # -*- coding: UTF-8 -*-
 import os
 import sys
+import subprocess
 
-a="\""+ sys.prefix + "\python.exe\"" +" -m pip install tensorflow -i http://mirrors.aliyun.com/pypi/simple/ --trusted-host=mirrors.aliyun.com"
-#a="\""+ sys.prefix + "\python.exe\"" +" -m pip install --upgrade tensorboard -i http://mirrors.aliyun.com/pypi/simple/ --trusted-host=mirrors.aliyun.com"
-#a="\""+ sys.prefix + "\python.exe\"" +" -m pip uninstall tensorboard"
+#升级setuptools与pip
+os.system('''"D:\Program Files\WinPython-64bit-3.6.1.0Qt5\python-3.6.1.amd64\python.exe" -m pip install --upgrade setuptools pip -i http://mirrors.aliyun.com/pypi/simple/ --trusted-host=mirrors.aliyun.com''')
+
+
+a="\""+ sys.prefix + "\python.exe\"" +" -m pip install fbprophet -i http://mirrors.aliyun.com/pypi/simple/ --trusted-host=mirrors.aliyun.com"
+#a="\""+ sys.prefix + "\python.exe\"" +" -m pip install --upgrade xlwings -i http://mirrors.aliyun.com/pypi/simple/ --trusted-host=mirrors.aliyun.com"
+#a="\""+ sys.prefix + "\python.exe\"" +" -m pip uninstall requests"
+#a="\""+ sys.prefix + "\python.exe\"" +" -m pip freeze > requirements.txt"
 #生成command命令,指定安装源为阿里云
 print("command命令是："+ a)
-os.system(a) #执行command
+
+status = subprocess.run(a,shell=True, capture_output=True,text=True )
+print(status.stdout)
+print(status.stderr)
