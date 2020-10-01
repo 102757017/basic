14d2134ee9c800e52c4d314011b8c913fffc400e
102757017 <sdf63fg@yeah.net>
2020-10-01 20:20:44 +0800

    阻塞模式/非阻塞模式
    Signed-off-by: 102757017 <sdf63fg@yeah.net>

--- shell/windows/shell管道.py
+++ shell/windows/shell管道.py
@@ -5,9 +5,14 @@ import subprocess
 
 
 
+#非阻塞模式
+os.popen("notepad")
+print("执行完毕")
 
-status = subprocess.run(["dir"],shell=True, capture_output=True,text=True )
+
+#会阻塞程序继续运行
+#执行单个命令不能使用["notepad"]，需要使用"notepad"
+status = subprocess.run(["notepad"],shell=True, capture_output=True,text=True )
 print(status.stdout)
 print(status.stderr)
-
-
+print("执行完毕")
