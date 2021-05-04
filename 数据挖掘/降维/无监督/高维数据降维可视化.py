0b59f283ed3b8b839a5f09976a2fa3c6d61f9f70
102757017 <sdf63fg@yeah.net>
2021-05-04 21:32:50 +0800

    注释修订
    Signed-off-by: 102757017 <sdf63fg@yeah.net>

--- 数据挖掘/降维/无监督/高维数据降维可视化.py
+++ 数据挖掘/降维/无监督/高维数据降维可视化.py
@@ -12,7 +12,7 @@ print(iris["target_names"])
 print(iris["data"].shape)
 
 #TSNE与keras中的池化层作用类似，是非线性降维，PCA是线性降维
-#TSNE期望的输入数据是np.array，会将数据降成两维
+#TSNE期望的输入数据是np.array，默认会将数据降成两维，指定n_components=3时可将数据降为三纬
 #T-SNE，PCA是无监督学习方法，输入的数据不需要标记，根据样本间的相似性对样本集进行自动分类
 X_tsne = TSNE(learning_rate=100).fit_transform(iris.data)
 
@@ -25,10 +25,12 @@ print("PCA降维后:",X_pca.shape)
 
 #使用T-SNE降维绘子图
 plt.subplot(121)
-plt.scatter(X_tsne[:, 0], X_tsne[:, 1], c=iris.target,label="T-SNE")
+plt.title("T-SNE")
+plt.scatter(X_tsne[:, 0], X_tsne[:, 1], c=iris.target)
 
 #使用PCA降维绘子图
 plt.subplot(122)
+plt.title("PCA")
 plt.scatter(X_pca[:, 0], X_pca[:, 1], c=iris.target,label="PCA")
 
 
