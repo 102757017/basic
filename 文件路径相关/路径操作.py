ea4a8cfc713a7b0f164cf76c5a14196aa56775d3
102757017 <sdf63fg@yeah.net>
2022-07-26 22:56:04 +0800

    os模块替换为Pathlib模块
    Signed-off-by: 102757017 <sdf63fg@yeah.net>

--- 文件路径相关/路径操作.py
+++ 文件路径相关/路径操作.py
@@ -3,6 +3,8 @@
 import os
 import sys
 import shutil
+from pathlib import Path
+
 
 print("当前操作系统的路径分隔符为:")
 print(os.path.sep)
@@ -16,54 +18,53 @@ print('\n')
 
 #  所有基于模块的使用到__file__属性的代码，在源码运行时表示的是当前脚本的绝对路径，但是用pyinstaller打包后就是当前模块的模块名（即文件名xxx.py）
 #  因此需要用以下代码来获取exe的绝对路径
-if getattr(sys, 'frozen', False):
-    bundle_dir = sys._MEIPASS
+if getattr(sys, "frozen", False):  # bundle mode with PyInstaller
+    os.environ["WICO_ROOT"] = sys._MEIPASS
 else:
-    bundle_dir = os.path.dirname(os.path.abspath(__file__))
-print(bundle_dir)
+    os.environ["WICO_ROOT"] = str(Path(__file__).parent)
+KV_DIR = f"{os.environ['WICO_ROOT']}"
+print(KV_DIR)
 
 
 print('当前脚本所在的目录：')
-a = os.path.dirname(a)
+a = Path(__file__).parent
 print(a)
 print('\n')
 
 print('上级目录：')
-a = os.path.dirname(a)
+a = Path(a).parent
 print(a)
 print('\n')
 
-print('用os.path.join构造路径:')
-a = os.path.join(a, 'xxx', "yyy")
-print(a)
-print('\n')
+print('构造路径:')
+a=a/"xxx"/"yyy"
 
-print('用os.path.sep构造路径:')
-a = os.path.sep.join([a, 'aaa', "bbb"])
 print(a)
 print('\n')
 
 
 print("当前脚本工作的目录路径:")
-path1 = os.getcwd()
+path1 = Path.cwd()
 print(path1)
 print('\n')
 
 print("切换工作目录")
 os.chdir(sys.path[0])
 print("当前脚本工作的目录路径:")
-path1 = os.getcwd()
+path1 = Path.cwd()
 print(path1)
 print('\n')
 
-if os.path.exists(sys.path[0]+os.path.sep+'temp'):
+root=Path(sys.path[0])
+
+if Path.exists(root/'temp'):
     #修改文件夹的名称
-    os.rename(sys.path[0]+os.path.sep+'temp',sys.path[0]+os.path.sep+'temp2')
+    Path.rename(root/'temp',root/'temp2')
     shutil.rmtree(sys.path[0]+os.path.sep+'temp2')
 
 print("在当前文件夹内创建temp目录:")
-path = sys.path[0]+os.path.sep+'temp'
-os.mkdir(path)
+path = root/'temp'
+Path.mkdir(path)
 print('\n')
 
 
@@ -90,25 +91,25 @@ print(list1)
 print('\n')
 
 
-print("在D盘创建log.txt的空文件:")
-open("D:\log.txt", "w+").close()
+print("在本目录创建log.txt的空文件:")
+open(root/"log.txt", "w+").close()
 print('\n')
 
 print("复制log.txt:")
-shutil.copy("D:\log.txt", "log.txt")
+shutil.copy(root/"log.txt", "log2.txt")
 print('\n')
 
 print("判断log.txt文件是否存在:")
-result = os.path.isfile("D:\log.txt")
+result = Path.is_file(root/"log.txt")
 print(result)
 print('\n')
 
-print("删除D盘的log.txt:")
-os.remove("D:\log.txt")
+print("删除本目录的log.txt:")
+os.unlink(root/"log.txt")
 print('\n')
 
 print("创建文件夹test:")
-os.mkdir("test")
+Path.mkdir(root/"test")
 print('\n')
 
 print("复制文件夹test为test2:")
@@ -117,42 +118,40 @@ print('\n')
 
 
 print("判断test文件夹是否存在:")
-result = os.path.isdir("test")
+result = Path.is_dir(root/"test")
 print(result)
 print('\n')
 
 
 print("删除test和test2文件夹:")
-shutil.rmtree("test")
-shutil.rmtree("test2")
+shutil.rmtree(root/"test")
+shutil.rmtree(root/"test2")
 print('\n')
 
 
 print("遍历指定目录下所有的文件和文件夹，包括子目录内的")
-list_dirs = os.walk(sys.path[0])
-for root, dirs, files in list_dirs:
-    for d in dirs:
-        print(os.path.join(root, d))
-    for f in files:
-        print(os.path.join(root, f))
+list_dirs = root.rglob('*')
+for files in list_dirs:
+    print(files)
 print('\n')
+
+
 
 print("遍历指定目录下所有的文件和文件夹，返回所有文件夹名词，不包括子目录内的")
-files = os.listdir(sys.path[0])
-for x in files:
-    if os.path.isdir(sys.path[0]+os.path.sep+ x):
-        print(x)
+list_dirs = root.glob('*')
+for files in list_dirs:
+    print(files)
 print('\n')
 
-print("遍历指定目录下所有的文件和文件夹，返回所有文件名称，不包括子目录内的")
-files = os.listdir(sys.path[0])
-for x in files:
-    if os.path.isfile(sys.path[0]+os.path.sep+ x):
-        # 分离文件名与扩展名，仅显示txt后缀的文件
-        if os.path.splitext(x)[1]=='.txt':
-            print(x)
+
+print("遍历所有txt文件，使用正则进行匹配")
+list_dirs = root.rglob('*.txt')
+for files in list_dirs:
+    print(files)
 print('\n')
+
 
 print("导入自定义模块:")
-sys.path.append(r'H:\学习资料\编程学习\pathon\基础操作\basic\文件读写')
+root = Path.cwd()
+sys.path.append(str(root.parent/"文件读写"))
 import 读写txt
