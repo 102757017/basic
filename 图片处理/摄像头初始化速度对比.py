cfd95275da04cd4517402a3705774cf9d0ccab34
sunny <hewei@imasenwh.com>
2025-10-15 08:41:33 +0800

    添加测试内容

--- 图片处理/摄像头初始化速度对比.py
+++ 图片处理/摄像头初始化速度对比.py
@@ -8,6 +8,9 @@ print("msmf test")
 t1=datetime.datetime.now()
 cap = cv2.VideoCapture(0)
 ret,frame=cap.read()
+codec = int(cap.get(cv2.CAP_PROP_FOURCC))
+fourcc_str = "".join([chr((codec >> 8 * i) & 0xFF) for i in range(4)])
+print(codec, fourcc_str)
 t2=datetime.datetime.now()-t1
 print(t2)
 cap.release()
@@ -30,12 +33,27 @@ print(t2)
 cap.release()
 
 
+t1=datetime.datetime.now()
+params = [
+    cv2.CAP_PROP_FRAME_WIDTH,1920,
+    cv2.CAP_PROP_FRAME_HEIGHT, 1080,
+    ]
+cap = cv2.VideoCapture(0,cv2.CAP_MSMF,params)
+ret,frame=cap.read()
+t2=datetime.datetime.now()-t1
+print(t2)
+cap.release()
 
 
 print("directshow test")
 t1=datetime.datetime.now()
 cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+mjpg_int = cv2.VideoWriter_fourcc(*'MJPG') 
+cap.set(cv2.CAP_PROP_FOURCC, mjpg_int) # directshow后端默认格式是YUY2
 ret,frame=cap.read()
+codec = int(cap.get(cv2.CAP_PROP_FOURCC))
+fourcc_str = "".join([chr((codec >> 8 * i) & 0xFF) for i in range(4)])
+print(codec, fourcc_str) 
 t2=datetime.datetime.now()-t1
 print(t2)
 cap.release()
